1c8aa9e6a1eb3f1c0dada49cebd069f0c6c66d9f
f7deat <f7deat@gmail.com>
2025-07-21 13:45:42 +0700

    feat: update header component with logo and enhance styling for better visibility

--- web/app/components/header.tsx
+++ web/app/components/header.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 
 const Header: React.FC = () => {
     return (
-        <header className="bg-white flex items-center justify-between md:h-22 h-16">
-            <div className="uppercase font-bold md:pl-10 pl-4 py-2 md:pr-20">
-                Đoàn Thanh niên <span className="text-red-600">Đại học Hải Phòng</span>
+        <header className="bg-white flex items-center justify-between md:h-22 h-16 shadow">
+            <div className="uppercase font-bold md:pl-10 p-4 md:pr-20">
+                <Link href="/">
+                    <img src="https://api.dtn.dhhp.edu.vn/imgs/logo.png" alt="Logo" className="h-full" />
+                </Link>
             </div>
             <div className="flex-1 hidden md:flex items-center justify-center nav-menu h-full">
                 <ul className="flex gap-8 z-10 relative">
@@ -18,12 +20,12 @@ const Header: React.FC = () => {
                     <li className="text-lg font-bold hover:text-red-600"><a href="/search"><FontAwesomeIcon icon={faSearch} className="w-5 h-5 inline" /></a></li>
                 </ul>
             </div>
-            <div className="h-full flex items-center gap-4 md:pl-20">
-                <Link href="/login" className="bg-red-600 hidden md:block text-white px-6 py-3 rounded hover:bg-red-700 transition-colors font-bold uppercase">Đăng nhập<FontAwesomeIcon icon={faExternalLink} className="ml-2 w-4 h-4 inline" /></Link>
-                <button type="button" className="bg-black text-white h-full w-22 flex items-center justify-center flex-col gap-3">
-                    <span className="border-b border-white w-10"></span>
-                    <span className="border-b border-white w-10"></span>
-                    <span className="border-b border-white w-10"></span>
+            <div className="h-full flex items-center gap-4 md:pl-20 pl-2">
+                <Link href="/login" className="bg-red-600 hidden md:block text-white px-4 md:px-6 py-3 rounded hover:bg-red-700 transition-colors font-bold uppercase">Đăng nhập<FontAwesomeIcon icon={faExternalLink} className="ml-2 w-4 h-4 inline" /></Link>
+                <button type="button" className="bg-black text-white h-full w-16 md:w-22 flex items-center justify-center flex-col gap-3">
+                    <span className="border-b border-white w-8 md:w-10"></span>
+                    <span className="border-b border-white w-8 md:w-10"></span>
+                    <span className="border-b border-white w-8 md:w-10"></span>
                 </button>
             </div>
         </header>
